1d0226e2ae1cd725be50d7e175acf3a885980522
Max Rudin <rudin.maksimka@bk.ru>
2023-01-09 15:14:58 +0500

    calendar change

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("header").classList.toggle("open")
     })
 })
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
